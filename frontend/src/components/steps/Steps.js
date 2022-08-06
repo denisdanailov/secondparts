98d92e34228595b45f7c6eab4c6ef640cf29631b
Denis Danailov <denis.danailov@gmx.de>
2022-08-06 10:37:28 +0200

    create steps section

---
+++ frontend/src/components/steps/Steps.js
@@ -0,0 +1,101 @@
+import "./Steps.css";
+
+export const Steps = () => {
+  return (
+    <section class="testimonial text-center">
+      <div class="container">
+        <div class="row text-center justify-content-center">
+          <div class="col-md-8 col-lg-4 mt-4 mt-lg-0">
+            <div class="fdb-box">
+              <div class="row no-gutters align-items-center">
+                <div class="col-3">
+                  <img
+                    alt="image"
+                    class="img-fluid rounded"
+                    src="./imgs/people/3.jpg"
+                  />
+                </div>
+                <div class="col-8 ml-auto">
+                  <p>
+                    <strong>Person Name</strong>
+                    <em>Co-founder at Company</em>
+                  </p>
+                </div>
+              </div>
+              <div class="row mt-4">
+                <div class="col-12">
+                  <p class="lead">
+                    "Far far away, behind the word mountains, far from the
+                    countries Vokalia. Separated they live in Bookmarksgrove
+                    right at the coast of the Semantics, a large language
+                    ocean."
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-8 col-lg-4 mt-4 mt-lg-0">
+            <div class="fdb-box">
+              <div class="row no-gutters align-items-center">
+                <div class="col-3">
+                  <img
+                    alt="image"
+                    class="img-fluid rounded"
+                    src="./imgs/people/3.jpg"
+                  />
+                </div>
+                <div class="col-8 ml-auto">
+                  <p>
+                    <strong>Person Name</strong>
+                    <em>Co-founder at Company</em>
+                  </p>
+                </div>
+              </div>
+              <div class="row mt-4">
+                <div class="col-12">
+                  <p class="lead">
+                    "Far far away, behind the word mountains, far from the
+                    countries Vokalia. Separated they live in Bookmarksgrove
+                    right at the coast of the Semantics, a large language
+                    ocean."
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-8 col-lg-4 mt-4 mt-lg-0">
+            <div class="fdb-box">
+              <div class="row no-gutters align-items-center">
+                <div class="col-3">
+                  <img
+                    alt="image"
+                    class="img-fluid rounded"
+                    src="./imgs/people/3.jpg"
+                  />
+                </div>
+                <div class="col-8 ml-auto">
+                  <p>
+                    <strong>Person Name</strong>
+                    <em>Co-founder at Company</em>
+                  </p>
+                </div>
+              </div>
+              <div class="row mt-4">
+                <div class="col-12">
+                  <p class="lead">
+                    "Far far away, behind the word mountains, far from the
+                    countries Vokalia. Separated they live in Bookmarksgrove
+                    right at the coast of the Semantics, a large language
+                    ocean."
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
