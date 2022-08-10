bf0d1b66393aba63d7d911a69700ad4a16ed020a
Denis Danailov <denis.danailov@gmx.de>
2022-08-10 10:03:35 +0200

    edit Admin controller url path

--- backend/src/main/java/de/secondparts/web/AdminPanelController.java
+++ backend/src/main/java/de/secondparts/web/AdminPanelController.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/admin")
 public class AdminPanelController {
 
     private final UserService userService;
